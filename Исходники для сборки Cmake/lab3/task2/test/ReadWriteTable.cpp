5d3c4930029d8ced8af36ae7184288ed7da24f07
Диана Абушаева <abu13.di@gmail.com>
2023-01-19 04:22:43 +0300

    Добавила исходники 3 лабы.

---
+++ Исходники для сборки Cmake/lab3/task2/test/ReadWriteTable.cpp
@@ -0,0 +1,47 @@
+#include "ReadWriteTable.h"
+using namespace std;
+
+void ReadTable(ifstream& input, vector<vector<string>>& inputAutomaton)
+{
+
+    string str;
+    int indexStr = 0;
+    while (getline(input, str))
+    {
+        indexStr += 1;
+        vector<string> arr;
+        std::string item;
+        if (str[str.size() - 1] == ';')
+        {
+            str = str + " ";
+        }
+        std::istringstream strStream(str);
+        while (getline(strStream, item, ';'))
+        {
+            if (item == "" || item == " " && indexStr != 1)
+            {
+                item = "-";
+            }
+            arr.push_back(item);
+        }
+        inputAutomaton.push_back(arr);
+    }
+
+}
+
+void WriteTable(ofstream& output, vector<vector<string>>& outputAutomaton)
+{
+    for (auto i = 0; i < outputAutomaton.size(); i++)
+    {
+        for (auto j = 0; j < outputAutomaton[i].size(); j++)
+        {
+            output << outputAutomaton[i][j];
+            if (j + 1 != outputAutomaton[i].size())
+            {
+                output << ";";
+            }
+        }
+        output << "\n";
+
+    }
+}
